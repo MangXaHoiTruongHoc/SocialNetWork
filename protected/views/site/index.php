48364d34d98ec95f6f00cdbe4620ca15603e5ae5
nguyenduc <nguyenducdn67@gmail.com>
2015-04-10 13:53:24 +0700

    !update

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,11 +1,7 @@
 <?php
 /* @var $this SiteController */
-<<<<<<< ca4016ea9a9d88087e28fb89a12ce0a10e05efcb
 
-$this->layout='//layouts/index';
-=======
 $this->layout="//layouts/index"; //dua layout theo muon
->>>>>>> 7106ab3bf734a787c8b781efa8fae5117c95ec00
 $this->pageTitle=Yii::app()->name;
 ?>
 <?php  
